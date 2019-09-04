aa309bd022f87d17e6b7c179f90d5e94269d5dfc
jcarrera94 <juankarrera@gmail.com>
2019-09-04 03:04:14 +0000

    creating is odd function

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function(num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(10));
+console.log(isOdd(2));
+console.log(isOdd(1));
+console.log(isOdd(67));
+console.log(isOdd(15));
